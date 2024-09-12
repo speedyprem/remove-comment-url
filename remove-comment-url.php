8de593bc2d2b691af1448899351a5a53a40fe352
Prem Tiwari <prem.tiwari@news.co.uk>
2024-09-12 19:43:30 +0530

    code rewrite

--- remove-comment-url.php
+++ remove-comment-url.php
@@ -14,18 +14,18 @@ Author URI: https://www.premtiwari.in/
  * @return Array
  */
 function rcu_disable_comment_url( $fields ) {
-    if ( isset( $fields['url'] ) ) {
-        unset( $fields['url'] );
-        return $fields;
-    }
+	if ( isset( $fields['url'] ) ) {
+		unset( $fields['url'] );
+		return $fields;
+	}
 }
 // Hook our function rcu_disable_comment_url with the filter comment_form_default_fields.
 add_filter( 'comment_form_default_fields', 'rcu_disable_comment_url' );
 
 // Filter to remove the existing links from the comments.
 add_filter( 'get_comment_author_url', function( $url, $comment_ID, $comment ) {
-    if ( ! is_admin() && $comment->user_id !== get_post()->post_author && ! user_can( $comment->user_id, 'manage_options' ) ) {
-        return '';
-    }
-    return $url;
+	if ( ! is_admin() && $comment->user_id !== get_post()->post_author && ! user_can( $comment->user_id, 'manage_options' ) ) {
+		return '';
+	}
+	return $url;
 }, 10, 3 );
